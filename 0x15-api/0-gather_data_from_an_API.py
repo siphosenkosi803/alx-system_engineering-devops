b225210829782e4ecf7f42ede3e3f0cdd96a2193
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-04-24 16:57:10 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""DataAPI module"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    api_url = 'https://jsonplaceholder.typicode.com'
+    employee_id = sys.argv[1]
+    r1 = requests.get('{}/users/{}'.format(api_url, employee_id))
+    employee_name = r1.json().get('name')
+    r2 = requests.get('{}/todos?userId={}'.format(api_url, employee_id))
+    responses = r2.json()
+    done_tasks = [t for t in responses if t.get('completed')]
+    print('Employee {} is done with tasks({}/{}):'.
+          format(employee_name, len(done_tasks), len(responses)))
+    for t in done_tasks:
+        print('\t {}'.format(t.get('title')))
