7a9498b69def9b8efb052690a804be06351c06ad
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-05-09 12:45:30 +0200

    Update 0-subs.py -PEP 8 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-""" advanced api script for quering the the Reddit API and returns the number of subs"""
+"""
+Returns the number of subs from Reddit API
+"""
+
 import requests
 import sys
 
 
 def number_of_subscribers(subreddit):
-    """this function will return the number of subs"""
-    _url_base_ = 'https://www.reddit.com'
-    _my_query_url_ = 'r/{}/about.json'.format(subreddit)
+    """Returns the number of subs"""
+    url_base = 'https://www.reddit.com'
+    query_url = 'r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "git/siphosenkosi803"}
-    _api_resp_ = requests.get(
-            url='{}/{}'.format(_url_base_, _my_query_url_),
-            allow_redirects=False,
-            headers=headers
-            )
-    if _api_resp_.status_code == 200:
-        JSON_api_data = _api_resp_.json().get("data")
-        if JSON_api_data:
-            return JSON_api_data.get("subscribers")
+    api_resp = requests.get(
+        url='{}/{}'.format(url_base, query_url),
+        headers=headers,
+        allow_redirects=False
+    )
+    if api_resp.status_code == 200:
+        json_api_data = api_resp.json().get("data")
+        if json_api_data:
+            return json_api_data.get("subscribers")
     return 0
 
+
+if __name__ == '__main__':
+    subreddit = sys.argv[1]
+    print("{:d}".format(number_of_subscribers(subreddit)))
