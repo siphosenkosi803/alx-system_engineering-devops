e692a827494d55a4356491437319453799f1053d
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-05-09 14:38:30 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,6 +6,7 @@ Returns the titles of the first 10 hot posts
 import requests
 import sys
 
+
 def top_ten(subreddit):
     """Returns the top 10 hot posts"""
     url_base = 'https://www.reddit.com'
