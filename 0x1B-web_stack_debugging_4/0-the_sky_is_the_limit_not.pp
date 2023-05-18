97dc20e4dd97ed8208fb91562eb85f3604e15041
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-05-18 13:22:21 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -4,4 +4,3 @@ exec { 'lets try increasing th number of workers':
   command => 'sudo sed -i "s/14096/" /etc/default/nginx; sudo service nginx restart',
   path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin:/bin:/sbin',
 }
-
