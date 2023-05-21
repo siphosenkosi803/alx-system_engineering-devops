c0080ce9a0a6d6172feba10ba5c359a623313731
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-05-21 15:05:56 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,20 @@
-# Fixing failed requests errors
+# new fix
 
-file { '/path/to/custom/nginx.conf':
+file { '/etc/nginx/nginx.conf':
   ensure  => file,
-  owner   => 'user',
-  group   => 'group',
+  owner   => 'root',
+  group   => 'root',
   mode    => '0644',
-  content => "worker_processes auto;\n\
-              events { worker_connections 100; };\n",
-} -> exec { 'lets restart nginx now':
-  command     => 'service nginx restart',
-  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+  content => "# Adjusting NGINX worker_processes and worker_connections\n\
+              worker_processes auto;\n\
+              events {\n\
+                worker_connections 100;\n\
+              };\n",
+  notify  => Exec['restart_nginx'],
+}
+
+exec { 'restart_nginx':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin:/sbin',
   refreshonly => true,
 }
