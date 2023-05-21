9a14a7a8b9bfa9438c2d775ef55384c2def7efc3
siphosenkosi803 <110998052+siphosenkosi803@users.noreply.github.com>
2023-05-21 15:11:21 +0200

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,6 @@
 # Changes the OS config for eay login
 
 exec { 'set better config':
-  command => 'sudo sed -i 
-             "s/nofile 5/nofile 50000/; s/nofile 4/nofile 40000/" /etc/security/limits.conf && sudo sysctl -w fs.file-max=100000 && sudo sysctl -p',
+  command => 'sudo sed -i "s/nofile 5/nofile 50000/; s/nofile 4/nofile 40000/" /etc/security/limits.conf && sudo sysctl -w fs.file-max=100000 && sudo sysctl -p',
   path    => '/usr/sbin:/usr/bin:/sbin:/bin',
 }
-
